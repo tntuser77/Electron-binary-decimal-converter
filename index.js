2b25229939be8bebf9db3df18ad3c844417c617c
Elijah Shope <eshope7489@gmail.com>
2023-03-22 14:24:10 -0400

    following the tutorial dot exe

--- index.js
+++ index.js
@@ -9,6 +9,19 @@ const createWindow = () => {
   win.loadFile('index.html')
 }
 
+
 app.whenReady().then(() => {
-  createWindow()
-})
+  createWindow();
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+      createWindow();
+    }
+  });
+});
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
